4a44c83f3fd4df43549a1bcd0b548f1e286afb22
Nada W. Abdelfattah <59118007+nabdelfattah@users.noreply.github.com>
2026-07-14 08:21:07 +0300

    fix: solve import bug

--- src/app/core/components/auth-layout/auth-layout.ts
+++ src/app/core/components/auth-layout/auth-layout.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-import { AuthTexTBox } from '../../../features/auth';
-import { RouterOutlet } from '../../../../../node_modules/@angular/router/types/_router_module-chunk';
+import { AuthTexTBox } from '@app/features/auth';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-auth-layout',
