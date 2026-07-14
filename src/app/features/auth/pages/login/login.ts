1fe79fb8fde72838de2a9a771422e604138f3605
Nada W. Abdelfattah <59118007+nabdelfattah@users.noreply.github.com>
2026-07-14 14:42:23 +0300

    feat: install primeng and try it

--- src/app/features/auth/pages/login/login.ts
+++ src/app/features/auth/pages/login/login.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
-
+import { ButtonModule } from 'primeng/button';
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [ButtonModule],
   templateUrl: './login.html',
 })
 export class Login {}
