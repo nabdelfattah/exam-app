4a465c8824852c2325af8ff5749088dd251886df
Nada W. Abdelfattah <59118007+nabdelfattah@users.noreply.github.com>
2026-07-14 12:12:26 +0300

    feat(auth): implement the auth-text-box component

--- src/app/features/auth/components/auth-text-box/auth-text-box.ts
+++ src/app/features/auth/components/auth-text-box/auth-text-box.ts
@@ -6,4 +6,23 @@ import { Logo } from '../../../../shared/components';
   imports: [Logo],
   templateUrl: './auth-text-box.html',
 })
-export class AuthTextBox {}
+export class AuthTextBox {
+  data = [
+    {
+      icon: 'assets/icons/brain.svg',
+      heading: 'Tailored Diplomas',
+      description: 'Choose from specialized tracks like Frontend, Backend, and Mobile Development.',
+    },
+    {
+      icon: '/assets/icons/book-open-check.svg',
+      heading: 'Focused Exams',
+      description: 'Access topic-specific tests including HTML, CSS, JavaScript, and more.',
+    },
+    {
+      icon: '/assets/icons/rectangle-ellipsis.svg',
+      heading: 'Smart Multi-Step Forms',
+      description: 'Choose from specialized tracks like Frontend, Backend, and Mobile Development.',
+    },
+  ];
+  item: any;
+}
