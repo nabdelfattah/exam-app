01e43b5ceb00e61e85bfb5d52e85d80d1d980e69
Nada W. Abdelfattah <59118007+nabdelfattah@users.noreply.github.com>
2026-07-26 21:18:20 +0300

    setup guards

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,14 @@ import { Routes } from '@angular/router';
 import { AuthLayout, Notfound } from '@core/components';
 import { MainLayoutComponent } from './core/components/main-layout/main-layout.component';
 import { DashboardComponent } from './features/dashboard/dashboard.component';
+import { authGuard } from './core/guards/auth-guard';
+import { guestGuard } from './core/guards/guest-guard';
 
 export const routes: Routes = [
   {
     path: '',
     component: MainLayoutComponent,
+    canActivate: [authGuard],
     children: [
       { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
       { path: 'dashboard', component: DashboardComponent, title: 'dashboard' },
@@ -15,6 +18,7 @@ export const routes: Routes = [
   {
     path: '',
     component: AuthLayout,
+    canActivate: [guestGuard],
     loadChildren: () => import('./features/auth/auth.route').then((c) => c.authRoutes),
   },
   { path: '**', component: Notfound },
