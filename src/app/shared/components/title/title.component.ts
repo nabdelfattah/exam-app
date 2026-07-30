1dc05b86e2cedf3ef1ad8a17f7dc5d0e17c90d13
Nada W. Abdelfattah <59118007+nabdelfattah@users.noreply.github.com>
2026-07-30 15:42:28 +0300

    implement the pages blue title component

---
+++ src/app/shared/components/title/title.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+import { ɵEmptyOutletComponent } from '@angular/router';
+
+@Component({
+  selector: 'app-title',
+  imports: [ɵEmptyOutletComponent],
+  templateUrl: './title.component.html',
+})
+export class TitleComponent {}
