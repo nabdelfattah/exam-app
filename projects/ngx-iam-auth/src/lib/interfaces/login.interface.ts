713ec82306e8952eba22c5eb74b41d06622c2877
Nada W. Abdelfattah <59118007+nabdelfattah@users.noreply.github.com>
2026-08-09 12:45:03 +0300

    replace any with LoginRowRes type

--- projects/ngx-iam-auth/src/lib/interfaces/login.interface.ts
+++ projects/ngx-iam-auth/src/lib/interfaces/login.interface.ts
@@ -12,3 +12,24 @@ export interface LoginRes {
   role: string;
   token: string;
 }
+export interface LoginRowRes {
+  status: boolean;
+  code: number;
+  payload: {
+    user: {
+      id: string;
+      username: string;
+      email: string;
+      phone: string;
+      firstName: string;
+      lastName: string;
+      profilePhoto: string;
+      emailVerified: boolean;
+      phoneVerified: boolean;
+      role: string;
+      createdAt: string;
+      updatedAt: string;
+    };
+    token: string;
+  };
+}
