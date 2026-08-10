884f74ffadefdebfa92358c7d55a5dbc6b690f6f
Nada W. Abdelfattah <59118007+nabdelfattah@users.noreply.github.com>
2026-08-10 19:00:36 +0300

    setup account pages and routes

---
+++ src/app/features/account/presentation/components/settings-layout/settings-layout.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-settings-layout',
+  imports: [],
+  templateUrl: './settings-layout.component.html',
+})
+export class SettingsLayoutComponent {}
---
+++ src/app/features/account/presentation/pages/change-password/change-password.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-change-password',
+  imports: [],
+  templateUrl: './change-password.component.html',
+})
+export class ChangePasswordComponent {}
